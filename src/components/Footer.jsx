2b60feab36ce369033556a27afd05cfe39cc20d4
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-09-23 10:47:11 +0100

    step 2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer()  {
+    return (
+    <footer>
+        <div>
+            <h2> The Brutal Martian Landscape</h2>
+            <h1> APOD PROJECT</h1>
+        </div>
+        <button>
+        <i className="fa-solid fa-circle-info"></i>
+        </button>
+    </footer>
+    )
+}
