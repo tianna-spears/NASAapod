96aa5628ee8b1ea39391847ff8a0132dcdbfb535
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-09-25 12:54:47 +0100

    Stop tracking .env file and add it to .gitignore

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
+/* eslint-disable react/prop-types */
 export default function Footer(props)  {
-    const { showModel, handleToggleModel } = props;
+    const { showModel, handleToggleModel, data } = props;
 
     return (
     <footer>
         <div className="bgGradient"></div>
         <div>
-            <h2> The Brutal Martian Landscape</h2>
+            <h2> {data.title || 'Loading title...'}</h2>
             <h1> APOD PROJECT</h1>
         </div>
         <button onClick= {handleToggleModel}>
