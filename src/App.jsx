c4c5f652266c1c195ab1180d7ef5c1eb07bcf7c2
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-09-26 00:40:06 +0100

    final steps

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import SideBar from "./components/SideBar";
 
 function App() {
-  const [data, setData] =useState(null);
-  const [loading, setLoading]= useState(false);
-  const [showModel, setShowModel] =useState(false);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showModel, setShowModel] = useState(false);
 
-  function handleToggleModel()  {
-    setShowModel(!showModel)
+  function handleToggleModel() {
+    setShowModel(!showModel);
   }
 
   useEffect(() => {
     async function fetchAPIData() {
-      const NASA_KEY= import.meta.env.VITE_NASA_API_KEY
-      const url = `https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`;      
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY
+      const url = 'https://api.nasa.gov/planetary/apod' + `?api_key=${NASA_KEY}`
+
+      const today = (new Date()).toDateString()
+      const localKey = `NASA-${today}`
+      setLoading(false);
+
+      if (localStorage.getItem(localKey)) {
+        const apiData = JSON.parse(localStorage.getItem(localKey))
+        setData(apiData);
+        setLoading(true);
+      }
 
       try {
-        const response= await fetch(url);
-        const apiData= await response.json();
+        const response = await fetch(url);
+        const apiData = await response.json();
+        localStorage.setItem(localKey, JSON.stringify(apiData));
         setData(apiData);
+        console.log('Fetched from API today');
       } catch (error) {
         console.log(error.message);
       }
     }
-    fetchAPIData();
-  }, [data]);
+    fetchAPIData()
+  }, [])
 
   return (
     <>
-      {data ? (<Main/>) : (
-        <div className="loadingState"> 
-          <i className="fa-solid fa-gear"></i>
+      {loading ? (<Main data={data} />) : (
+        <div className="loadingState">
+          <i className="fas fa-gear"></i>        
         </div>
       )}
       {showModel && (
-        <SideBar
-        handleToggleModel={handleToggleModel} /> 
+        <SideBar data={data} handleToggleModel={handleToggleModel} />
       )}
       {data && (
-      <Footer 
-      data={data} 
-      handleToggleModel={handleToggleModel} />
+        <Footer data={data} handleToggleModel={handleToggleModel} />
       )}
     </>
   )
